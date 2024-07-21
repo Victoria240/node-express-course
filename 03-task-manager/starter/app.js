33989ff97a6c83c6e7bc9781a96932c3cfe109d2
Victoria240 <taiwovictoria024@gmail.com>
2024-07-21 02:25:27 +0100

    Task Manager API - Part 1
    Review of JavaScript Array Methods
    Connecting to a Mongo Database
    Externalizing Secrets in a .env File
    Using the Mongoose NPM Package
    Mongo Models for Schema

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,40 @@
-console.log('Task Manager App')
+
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect')
+require ('dotenv').config()
+
+// Middleware
+app.use(express.json());
+
+// Route for the root URL
+app.get('/', (req, res) => {
+    res.send('Welcome to the Task Manager App');
+});
+
+// Route for /hello
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App');
+});
+
+// Tasks routes
+app.use('/api/v1/tasks', tasks);
+
+const port = 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, 
+            console.log(`Server is listening on port ${port}...`)
+        );
+    
+
+    } catch (error) {
+      console.log (error)
+        
+    }
+}
+
+start()
